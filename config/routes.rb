8db064d404350bae0effb76f3de38260a24f0917
Miguel Ugalde <migugalde@berkeley.edu>
2017-08-03 19:51:03 +0000

    Adding in routes for each view

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,27 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
-  resources :game
+  root 'game#create'
   
+  get 'game/board', to: 'game#board', as: 'board'
+  
+  get 'game/join', to: 'game#join', as: "join"
+  
+  get 'game/start', to: 'game#start', as: "start"
+  
+  get 'game/kingdom', to: 'game#kingdom', as: "kingdom"
+  
+  get 'game/lose', to: 'game#lose', as: "lose"
+  
+  get "game/night_shade", to: 'game#night_shade', as: "night_shade"
+  
+  get "game/play_turn", to: "game#play_turn", as: "play_turn"
+  
+  get "game/win", to: "game#win", as: "win"
+  
+  get "game/lose", to: "game#lose", as: "lose"
+  
+  get "game/message", to: "game#message", as: "message"
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
