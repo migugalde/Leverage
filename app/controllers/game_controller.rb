6879990f02823f02728a7a91a435b9c7e96ae095
Miguel Ugalde <migugalde@berkeley.edu>
2017-08-03 17:41:07 +0000

    Add: adding in initial low grain controllers, and views that we may need

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,40 @@
+class GameController < ApplicationController
+  
+  #when player initially instantiates a game
+  def create
+  end 
+  
+  #here players will specify name and other stuff
+  def join
+  end
+  
+  #this route is triggered when everyone has entered the game and group is ready to play
+  def start
+  end
+  
+  #here we have logic about who's turn it is
+  def board
+  end
+  
+  #going into a kingdom
+  def kingdom
+  end
+  
+  #clicking on your messages
+  def message
+  end
+  
+  #will prob want to do this with ajax
+  def quiz
+  end
+  
+  
+  #will most likely end up combining these two as a method and just redirecting render to a win/lose page
+  def win
+  end
+  
+  def lose
+  end
+  
+  
+end
