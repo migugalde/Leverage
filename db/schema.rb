3c29bae00f3242de731f4dd04999b472d0ea9b15
Hana Hyder <hhyder@berkeley.edu>
2017-08-07 04:10:01 +0000

    Added nightshade character to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807040410) do
+ActiveRecord::Schema.define(version: 20170807040930) do
 
   create_table "games", force: :cascade do |t|
     t.string   "formula"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170807040410) do
     t.integer  "character"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "nightshade"
   end
 
 end
